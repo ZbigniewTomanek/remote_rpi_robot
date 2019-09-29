952baa139ad9870f802e758fda2cbff70fe284b0
ZbigniewTomanek <zbigniewtomanekgit@gmail.com>
2019-09-29 22:38:34 +0200

    teeeest

--- drive_test.py
+++ drive_test.py
@@ -1,7 +1,12 @@
 from robot_hardware import drive_control
 import time
 
-if __name__ == '__main__':
+
+def ride_forward_test():
     drive_control.forward()
     time.sleep(1)
     drive_control.stop()
+
+
+if __name__ == '__main__':
+    ride_forward_test()
